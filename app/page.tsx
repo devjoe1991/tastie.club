f7a16c4dab753205c8e263672153e76645e023e2
devjoe1991 <dev.joe91@gmail.com>
2025-08-13 20:09:47 +0100

    refactor: Remove ExperienceGallery component from Home page
    This commit removes the ExperienceGallery component from the Home page, streamlining the layout and focusing on the remaining core sections.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from './components/HeroSection';
 import Header from './components/Header';
 import HowItWorksSection from './components/HowItWorksSection';
-import ExperienceGallery from './components/ExperienceGallery';
 
 export default function Home() {
   return (
@@ -9,7 +8,6 @@ export default function Home() {
       <Header />
       <HeroSection />
       <HowItWorksSection />
-      <ExperienceGallery />
     </main>
   );
 }
